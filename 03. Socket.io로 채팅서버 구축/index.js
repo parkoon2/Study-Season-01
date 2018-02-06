7858d9322c7b5ab3527cde54ecaee7197483de64
Kjina <cream0329@naver.com>
2018-02-06 22:13:55 +0900

    Add files via upload

--- 03. Socket.io로 채팅서버 구축/index.js
+++ 03. Socket.io로 채팅서버 구축/index.js
@@ -6,9 +6,9 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on('connection', function(socket){ // 서버와 클라이언트가 연결되었을 때 실행
+  socket.on('chat message', function(msg){ // 클라이언트에서 서버로 오는 요청을 처리하는 이벤트 리스너
+    io.emit('chat message', msg); // 클라이언트로 데이터를 보내는 부분
   });
 });
 
