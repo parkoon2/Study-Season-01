c5819048d87f1ffad451fc3f37747f56fbfdd6a8
jinaaa.kim <jinaaa.kim@GNRHQ10-NE100M0.LGE.NET>
2018-03-09 16:25:53 +0900

    redis추가해야됨

---
+++ 05. Redis 서버 구축/ssl_socket_mongo_redis/public/js/chat.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	const socket = io.connect();
+	
+	const $inner_chat = $( '#inner_chat' );
+	socket.on( 'data_send', function( data ) {
+		if ( data.length ) {
+			for ( let x = 0; x < data.length; x ++ ) {
+				$inner_chat.append( '<p>' + data[x].name + ' : ' + data[x].message + ' </p> ' );
+			}
+		}
+	});
+	
+	const $name = $( '#name' );
+	const $sendButton = $( '#sendButton' );
+	const $message = $( '#message' );
+	$sendButton.click( function() {
+		let name = $name.val();
+		let message = $message.val();
+		socket.emit( 'input', { name : name , message : message } );
+		$message.val('');
+	});
+	$message.keyup( function(e) {
+		if ( e.which == 13 ) {
+			let name = $name.val();
+			let message = $message.val();
+			socket.emit( 'input', { name : name , message : message } );
+			$message.val('');
+		}
+	});
+	
+	const $clearButton = $( "#clearButton" );
+	$clearButton.click( function() {
+		socket.emit( 'clear' );
+	});
+	socket.on( 'cleared', function( data ) {
+		$inner_chat.empty();
+	});
+	
+});
---
+++ 05. Redis 서버 구축/ssl_socket_mongo_redis/server.js
@@ -0,0 +1,66 @@
+const https = require('https');
+const express = require( 'express' );
+const redis = require( 'redis' );
+const mongoose = require( 'mongoose' );
+const path = require( "path" );
+const fs = require('fs');
+const app = express();
+
+mongoose.connect( 'mongodb://localhost:27017/mongochat' );
+const db = mongoose.connection;
+const options = {
+	key: fs.readFileSync('./https/key.pem'),
+	cert: fs.readFileSync('./https/cert.pem')
+};
+
+app.use( express.static( path.join( __dirname, './public' ) ) );
+
+let messages = [];
+let name;
+const message = redis.createClient();
+
+app.set( 'views', path.join( __dirname, './views' ) );
+app.set( 'view engine', 'ejs' );
+
+app.get( '/', function( req, res ) {
+ res.render( 'index', { messages : messages } );
+});
+
+/*const server = https.createServer(options, app).listen(3000, function() {
+  console.log("HTTPS server listening on port " + 3000);
+});*/
+const server = app.listen( 3000, function() {
+	console.log( "listening on port 3000" );
+});
+
+
+const client = require( 'socket.io' ).listen( server ).sockets;
+
+client.on( 'connection', function() {
+	
+	client.on( 'connection', function( socket ) {
+		console.log("socket connect");
+		let chat = db.collection( 'chats' );
+
+		//limit( 100 ) : 보여주고싶은 데이터 갯수 제한(100개) 
+		chat.find().limit( 100 ).sort({ _id : 1 }).toArray( function( err, res ) {
+			if ( err ) {
+				throw err;
+			}
+			socket.emit( 'data_send', res );
+		});
+
+		socket.on( 'input', function( data ) {
+			chat.insert({ name : data.name }, function() {
+				client.emit( 'data_send', [ data ] );
+			});
+		});
+		
+		socket.on( 'clear', function( data ) {
+			chat.remove( {}, function() {
+				socket.emit( 'cleared' );
+			});
+		});
+	});
+});
+
