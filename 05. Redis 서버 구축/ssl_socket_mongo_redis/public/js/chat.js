7e29c4f29788db4f1e55bb201d8da414895e9662
jinaaa.kim <jinaaa.kim@GNRHQ10-NE100M0.LGE.NET>
2018-03-14 13:41:15 +0900

    1차

--- 05. Redis 서버 구축/ssl_socket_mongo_redis/public/js/chat.js
+++ 05. Redis 서버 구축/ssl_socket_mongo_redis/public/js/chat.js
@@ -1,31 +1,36 @@
-var room = "room_one";
-$(document).ready(function() {
+var room = 'room_one';
+$( document ).ready( function() {
 	
 	const socket = io.connect();
 	
-	socket.on("displayMessage", function (data) {
-		let num = $("#inner_chat").children("p").length;
-		console.log("num",num); // 무조건 10?
-		let messageString = "<p>"+data+"</p>";
-		console.log("data",data); // submit 버튼 클릭시 현재 메시지
-		if ( num >= 10 ) { $("#inner_chat").children("p:first").remove();}
-		$("#inner_chat").append(messageString);
+	const $inner_chat = $( '#inner_chat' );
+	socket.on( 'displayMessage', function ( data ) {
+		let num = $inner_chat.children( 'p' ).length; //무조건 10?? index.ejs 에 박혀있음
+		console.log(num);
+		let messageString = '<p>' + data + '</p>';
+		console.log( 'data', data); // submit 버튼 클릭시 현재 메시지
+		if ( num >= 18 ) { $inner_chat.children( 'p:first' ).remove(); }
+		$inner_chat.append( messageString );
 	});
 	
-	socket.on("clear_room", function(){
-		$("#inner_chat").children("p").html("&nbsp;");
+	socket.on( 'clear_room', function() {
+		$inner_chat.children( 'p' ).html( '&nbsp;' );
 	});
 	
-	$("#sendButton").click(function(e){
+	const $sendButton = $( '#sendButton' );
+	$sendButton.click( function( e ) {
+		e.preventDefault();
 		let message = $( '#message' ).val();
-		if (message != "") socket.emit("chatMessage", [room, message]);
-		$( '#message' ).val("");
+		if ( message != '' ) socket.emit( "chatMessage", [ room, message ] );
+		$( '#message' ).val('');
 	});
 	
-	$(".roomButton").click(function(e){
+	const $roomButton = $( '.roomButton' );
+	$roomButton.click( function( e ) {
+		e.preventDefault();
 		let old_room = room;
-		window.room = $(this).data("room");
-		let join_event = "join_"+room;
+		window.room = $( this ).data( 'room' );
+		let join_event = 'join_' + room;
 		if( old_room !== window.room ) {
 			socket.emit( join_event, [ old_room, room ] );
 		} else {
--- 05. Redis 서버 구축/ssl_socket_mongo_redis/server.js
+++ 05. Redis 서버 구축/ssl_socket_mongo_redis/server.js
@@ -1,17 +1,17 @@
-const http = require('http');
+const http = require( 'http' );
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
-const path = require( "path" );
-const fs = require('fs');
+const path = require( 'path' );
+const fs = require( 'fs' );
 const app = express();
-const redis = require("redis");
+const redis = require( 'redis' );
 const clientRoom = redis.createClient();
-let rooms = ["room_one", "room_two", "room_three", "room_four", "room_five"];
+let rooms = [ 'room_one', 'room_two', 'room_three', 'room_four', 'room_five' ];
 mongoose.connect( 'mongodb://localhost:27017/mongochat' );
 const db = mongoose.connection;
 const options = {
-	key: fs.readFileSync('./https/key.pem'),
-	cert: fs.readFileSync('./https/cert.pem')
+	key: fs.readFileSync( './https/key.pem' ),
+	cert: fs.readFileSync( './https/cert.pem' )
 };
 app.use( express.static( path.join( __dirname, './public' ) ) );
 app.set( 'views', path.join( __dirname, './views' ) );
@@ -19,57 +19,61 @@ app.set( 'view engine', 'ejs' );
 app.get( '/', function( req, res ) {
 	res.render( 'index' );
 });
-clientRoom.on("error", function (err) {console.log("Error " + err);});
+
+clientRoom.on( 'error', function( err ) { console.log("Error " + err ); } );
+
 const server = app.listen( 3000, function() {
-	console.log( "listening on port 3000" );
+	console.log( 'listening on port 3000' );
 });
+
 const client = require( 'socket.io' ).listen( server ).sockets;
 
 
-let storeMessage = function(room, message) {
+let storeMessage = function( room, message ) {
 	console.log("room!!!",room); // submit 버튼으로 클릭시 현재 방 번호
 	console.log("message!!!",message); //submit 버튼으로 클릭시 현재 쓴 메시지
 	/** .lpush -> 문자열 배열을 redis queue 에 넣음*/
-	clientRoom.lpush(room, message, function (err, res) {
+	clientRoom.lpush( room, message, function ( err, res ) {
 		// room 목록이 0 번째 ~ 9 번째까지만 남겨놈
-		clientRoom.ltrim(room, 0, 9);
+		clientRoom.ltrim( room, 0, 17 );
 	});
-	clientRoom.in(room).emit("displayMessage", message);
+	client.in( room ).emit( 'displayMessage', message );
 }
 
-let emitMessages = function(room, socket){
-	clientRoom.lrange(room, 0, -1, function(err, messages){
+let emitMessages = function( room, socket ) {
+	console.log( 'room ???????? ', room );
+	clientRoom.lrange( room, 0, -1, function ( err, messages ) {
 		//reverse() 는 배열을 반전 시키는 메소드 (역순배열)
-		messages = messages.reverse();
+		//messages = messages.reverse();
 		messages.forEach(function(message) {
 			socket.emit("displayMessage", message); // 이곳의 메시지는 이미 저장되어 있는 메시지
+			console.log("message ????? ",message);
 		});
 	});
 }
 
 
-client.on("connection", function (socket) {
-	console.log("connect");
-	socket.join("room_one");
+client.on( 'connection', function( socket ) {
+	console.log( 'connect' );
+	let chat = db.collection( 'chats' );
+	socket.join( 'room_one' );
 	
-	emitMessages("room_one", socket);
+	emitMessages( 'room_one', socket );
 	
-	socket.on("chatMessage", function(data){
-	  storeMessage(data[0], data[1]);
-	  console.log("data[0]",data[0]); //submit 버튼 클릭시 현재 방번호
-	  console.log("data[1]",data[1]); //submit 버튼 클릭시 현재 적힌 메시지
+	socket.on( 'chatMessage', function( data ) {
+	  storeMessage( data[0], data[1] );
+	  console.log( 'data[0]', data[0] ); //submit 버튼 클릭시 현재 방번호
+	  console.log( 'data[1]', data[1] ); //submit 버튼 클릭시 현재 적힌 메시지
 	});
 	
-	for (let i = 0; i < rooms.length; i++) {
+	for ( let i = 0; i < rooms.length; i++ ) {
 		let room = rooms[i];
-		let join_event = "join_"+room;
-		socket.on(join_event, function(rooms){
-			socket.leave(rooms[0]); // 클릭 하기 전 현재 방
-			console.log("rooms[0]",rooms[0]);
+		let join_event = 'join_' + room;
+		socket.on( join_event, function( rooms ) {
+			socket.leave( rooms[0] ); // 클릭 하기 전 현재 방
 			socket.join(rooms[1]); // 클릭한 방
-			console.log("rooms[1]",rooms[1]);
-			socket.emit("clear_room");
-			emitMessages(rooms[1], socket);
+			socket.emit( 'clear_room' );
+			emitMessages( rooms[1], socket );
 		});	
 	}
 	
